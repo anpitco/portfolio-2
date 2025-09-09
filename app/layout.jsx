50496d4283a1f84f46762e4fd70c6650c64c4712
Anna Pitcock <anna.pitcock@gmail.com>
2025-09-09 14:55:54 -0600

    Chaning home page

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,8 +4,8 @@ import { Header } from '../components/header';
 
 export const metadata = {
     title: {
-        template: '%s | Netlify',
-        default: 'Netlify Starter'
+        template: '%s | Anna Pitcock',
+        default: 'Anna Pitcock'
     }
 };
 
