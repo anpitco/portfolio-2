c617a4e6d5c79df6ea20f511b46b4d02537ab5b1
Anna Pitcock <anna.pitcock@gmail.com>
2025-12-16 15:00:24 -0700

    Added Kinective and Oracle experience

--- app/experience/page.jsx
+++ app/experience/page.jsx
@@ -18,18 +18,34 @@ const sampleImageSrcSet = [640, 1280, 2048]
     })
     .join(', ');
 
-const nextImageSnippet = `
-When running on Netlify, \`next/image\` is automatically set-up to use Netlify Image CDN for optimized images.
-
-~~~jsx
-import Image from 'next/image';
-
-// In your component
-<Image src="/images/corgi.jpg" alt="Corgi" /* ... additional props */ />
-~~~
+const kinectiveJob = `
+* Create and manage cloud infrastructure and software, OS configuration and deployment, databases and internal software development tools
+* Create and deploy internal software stack to cloud and on-prem environments
+* Assist software development team with software, database, and network configuration
 `;
 
-const originalVsCdnSnippet = `
+const oracleJob = `
+• Contributed to the development of two major services: the Marketplace Project and the Certificate-Based Resource Principal Service,
+both critical to OCI’s strategic initiatives.
+o The Marketplace project is a curated digital storefront where customers can discover, procure, and deploy third-party
+software, data, and services that integrate seamlessly with Oracle's infrastructure.
+o The Certificate-Based Resource Principal Service project utilized digital certificates to authenticate and authorize non-
+human entities, when needing to access cloud resources. This method provides a secure and automated way for these
+entities to interact with cloud APIs and services without requiring human intervention or traditional username/password
+credentials.
+• Designed and implemented numerous RESTful API endpoints to support scalable service operations.
+• Utilized Java and Python for backend service development and Terraform for infrastructure as code, enabling reliable deployment and
+management of OCI resources.
+• Developed and integrated detailed service metrics and alarms for enhanced observability and monitoring using Prometheus. Created
+subsequent dashboards in Grafana to track metrics.
+• Authored internal documentation to support service adoption, maintenance, and onboarding using Confluence and runbooks.
+• Provided on call support every 2 months on a rotational basis supporting production regions and users.
+• Participated in deployment rotation to release latest code to development, integration, and production regions using Shepherd.
+• Agile experience includes participating in daily stand-ups, sprint planning, and retrospectives.
+• Skilled in using Jira for Agile project management, including managing backlogs, sprints, and issue tracking.
+`;
+
+const exampleCodeSnipet = `
 In the code below, a regular \`<img>\` tag is used in both cases for a framework-agnostic example. 
 Other than using \`next/image\` or rolling your own \`<img>\` tags, you can also use the excellent [unpic-img](https://unpic.pics/).
 
@@ -55,20 +71,21 @@ export default function Page() {
     return (
         <div className="flex flex-col gap-12 sm:gap-16">
             <section>
-                <ContextAlert
+                {/* <ContextAlert
                     addedChecksFunction={(ctx) => {
                         return ctx === 'dev' ? devModeWarning : null;
                     }}
                     className="mb-6"
-                />
+                /> */}
                 <h1>Experience</h1>
             </section>
             <section>
-                <h2 className="mb-6">Kinective Jan 2026 - Present</h2>
+                <h2 className="mb-6">Kinective              Jan 2026 - Present</h2>
+                <Markdown content={kinectiveJob} className="mb-12" />
             </section>
             <section>
-                <h2 className="mb-6">Original vs. optimized image: can you tell the difference?</h2>
-                <Markdown content={originalVsCdnSnippet} className="mb-8" />
+                <h2 className="mb-6">Oracle                 September 2023 - September 2025</h2>
+                <Markdown content={oracleJob} className="mb-12" />
                 <figure
                     className="relative grid w-full overflow-hidden border-2 border-white rounded-lg select-none diff aspect-3/2"
                     tabIndex="0"
